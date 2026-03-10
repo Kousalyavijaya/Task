ca542a92c215d98e13b604f4f50b8578d4284f77
Kousalya.V <vkousalya538@gmail.com>
2026-03-10 11:06:42 +0530

    Added portfolio website

---
+++ functions/portfolio/dashboard/marks.py
@@ -0,0 +1,20 @@
+from django.db import models
+from .persondetails import PersonDetails
+
+
+class Marks(models.Model):
+    person = models.ForeignKey(
+        PersonDetails,
+        on_delete=models.CASCADE,
+        related_name="marks"
+    )
+
+    subject = models.CharField(max_length=100)
+    marks_obtained = models.FloatField()
+    total_marks = models.FloatField(default=100)
+
+    def percentage(self):
+        return (self.marks_obtained / self.total_marks) * 100
+
+    def __str__(self):
+        return f"{self.person.name} - {self.subject}"
