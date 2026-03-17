4824c027ffc067a9afdfda8d70842231e2d813bb
Kousalya.V <vkousalya538@gmail.com>
2026-03-17 19:56:01 +0530

    Add React student app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+
+      <h2 style={{textAlign:"center"}}>Welcome to Student App</h2>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function StudentFooter() {
+  return (
+    <footer style={{ background: "#1e90ff", color: "white", padding: "10px", marginTop: "20px", textAlign:"center" }}>
+      <p>© 2026 Student App | All Rights Reserved</p>
+    </footer>
+  );
+}
+
+export default StudentFooter;
